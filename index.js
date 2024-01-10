1998019a3864ed72ef03d3431fa1f72452811b97
Mayya Matueva <mayamatueva@gmail.com>
2024-01-10 11:06:00 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// In this file we can write our code
+
+const firstName = "Niklas";
+const lastName = "Fähnrich";
+
+const greeting = `Hello ${firstName} ${lastName}! How are you doing?`;
+
+console.log(greeting); // Hello Niklas Fähnrich! How are you doing?
