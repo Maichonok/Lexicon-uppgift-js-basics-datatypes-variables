0df6a6bbb8e0873c0b06883d9c1d2277976dae0e
Mayya Matueva <mayamatueva@gmail.com>
2024-01-10 11:44:17 +0100

    add data types and operator section

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
 // In this file we can write our code
-
-const firstName = "Niklas";
-const lastName = "Fähnrich";
-
-const greeting = `Hello ${firstName} ${lastName}! How are you doing?`;
-
-console.log(greeting); // Hello Niklas Fähnrich! How are you doing?
